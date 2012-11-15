e1a42acaf783122aad1bfab93e8963b9fefd0734
Andrew Shaw <zeckal4@gmail.com>
2012-11-14 21:21:24 -0700

    all menu images loaded into correct places, no interaction yet, added menu background image and the black puck image, I'd like to redraw these at some point

--- gen/com/example/shuffleboard/R.java
+++ gen/com/example/shuffleboard/R.java
@@ -13,9 +13,12 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int mainmenu=0x7f020002;
+        public static final int menuslider=0x7f020003;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int graphics_holder=0x7f070000;
+        public static final int menu_settings=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main_menu=0x7f030000;
--- src/com/example/shuffleboard/MainMenuActivity.java
+++ src/com/example/shuffleboard/MainMenuActivity.java
@@ -1,15 +1,54 @@
 package com.example.shuffleboard;
 
-import android.os.Bundle;
+
+import java.util.ArrayList;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
+import android.widget.FrameLayout;
 
 public class MainMenuActivity extends Activity {
-
+	//public static final int TARGETS=1;
+	//private int[] images = new int[TARGETS];
+	//private ArrayList<Target> targets = new ArrayList<Target>(TARGETS);
+	//Puck SliderPuck = new Puck(this);
+	public static final int height = 775;
+	public static final int heightOffset = 100;
+	public static final int width = 500;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        FrameLayout frame = (FrameLayout) findViewById(R.id.graphics_holder);
+        /*
+			images[0] = R.drawable.ic_mainmenu;            
+				for (int i = 0;i< TARGETS;i++){
+				targets.add(new Target(this));
+				targets.get(i).setBitmap(images[i]);
+				targets.get(i).setx(0);
+				targets.get(i).sety(0);
+				frame.addView(targets.get(i));
+			}
+		*/
+		Target Background = new Target(this);
+		Background.setBitmap(R.drawable.mainmenu);
+		Background.setX(0);
+		Background.setY(0);
+		frame.addView(Background);
+		
+		Target MenuPuck = new Target(this);
+		MenuPuck.setBitmap(R.drawable.menuslider);
+		MenuPuck.setX(width/2 - MenuPuck.getwidth()/2);
+		MenuPuck.setY(height/2 - MenuPuck.getheight()/2 + heightOffset);
+		System.out.println("Height: " + height);
+		System.out.println("Width: " + width);
+		System.out.println("puck height: " + MenuPuck.getheight());
+		System.out.println("puck width: " + MenuPuck.getwidth());
+		frame.addView(MenuPuck);
+		
     }
 
     @Override
