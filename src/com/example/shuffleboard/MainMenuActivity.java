bbb9c0d61560a0ade53140d7b92ea776a0855e65
Andrew Shaw <zeckal4@gmail.com>
2012-11-16 00:01:14 -0700

    added functionality to make the puck follow the cursor on the menu screen

--- src/com/example/shuffleboard/MainMenuActivity.java
+++ src/com/example/shuffleboard/MainMenuActivity.java
@@ -3,50 +3,43 @@ package com.example.shuffleboard;
 
 import java.util.ArrayList;
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.Window;
 import android.widget.FrameLayout;
 
 public class MainMenuActivity extends Activity {
-	//public static final int TARGETS=1;
-	//private int[] images = new int[TARGETS];
-	//private ArrayList<Target> targets = new ArrayList<Target>(TARGETS);
-	//Puck SliderPuck = new Puck(this);
+	private Target selected = null;
+	private Target MenuPuck;
+	private Target Background;
+	private ArrayList<Point> VelocityStack = new ArrayList<Point>(5);
 	public static final int height = 775;
 	public static final int heightOffset = 100;
 	public static final int width = 500;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
     	this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    	
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main_menu);
         FrameLayout frame = (FrameLayout) findViewById(R.id.graphics_holder);
-        /*
-			images[0] = R.drawable.ic_mainmenu;            
-				for (int i = 0;i< TARGETS;i++){
-				targets.add(new Target(this));
-				targets.get(i).setBitmap(images[i]);
-				targets.get(i).setx(0);
-				targets.get(i).sety(0);
-				frame.addView(targets.get(i));
-			}
-		*/
-		Target Background = new Target(this);
+        
+    	MenuPuck = new Target(this);
+    	Background = new Target(this);
+        
 		Background.setBitmap(R.drawable.mainmenu);
 		Background.setX(0);
 		Background.setY(0);
 		frame.addView(Background);
 		
-		Target MenuPuck = new Target(this);
 		MenuPuck.setBitmap(R.drawable.menuslider);
 		MenuPuck.setX(width/2 - MenuPuck.getwidth()/2);
 		MenuPuck.setY(height/2 - MenuPuck.getheight()/2 + heightOffset);
-		System.out.println("Height: " + height);
-		System.out.println("Width: " + width);
-		System.out.println("puck height: " + MenuPuck.getheight());
-		System.out.println("puck width: " + MenuPuck.getwidth());
 		frame.addView(MenuPuck);
 		
     }
@@ -56,4 +49,39 @@ public class MainMenuActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main_menu, menu);
         return true;
     }
+    
+    
+    @Override  
+	public boolean onTouchEvent(MotionEvent event) { 
+		FrameLayout frame = (FrameLayout) findViewById(R.id.graphics_holder);
+		float X = event.getX();
+		float Y = event.getY()-48;
+		
+		if(event.getAction() == android.view.MotionEvent.ACTION_DOWN){
+			System.out.println("action down triggered");
+			if(MenuPuck.isInside(X,Y)){
+				selected = MenuPuck;
+				System.out.println("action down triggered");
+			}
+			return true;
+		} else if (event.getAction() == android.view.MotionEvent.ACTION_UP){
+			System.out.println("action up triggered");
+			if(MenuPuck.isInside(X,Y)){
+				selected = null;
+				System.out.println("action up triggered");
+			}
+			return true;
+		} else if (event.getAction() == android.view.MotionEvent.ACTION_MOVE){
+			System.out.println("action move triggered");
+			if(MenuPuck.isInside(X,Y)){
+				MenuPuck.setX(X - MenuPuck.getwidth()/2);
+				MenuPuck.setY(Y - MenuPuck.getheight()/2);
+				System.out.println("action move triggered");
+			}
+			return true;
+		}
+		
+		return false; 
+	}
+    
 }
