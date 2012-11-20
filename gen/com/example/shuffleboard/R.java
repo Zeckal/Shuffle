7d6fa49d183945a61ac60188df462e327b924440
Andrew Shaw <zeckal4@gmail.com>
2012-11-20 08:42:23 -0700

    all basic functionality

--- gen/com/example/shuffleboard/R.java
+++ gen/com/example/shuffleboard/R.java
@@ -11,25 +11,38 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int mainmenu=0x7f020002;
-        public static final int menuslider=0x7f020003;
+        public static final int blueslider=0x7f020000;
+        public static final int board=0x7f020001;
+        public static final int board1=0x7f020002;
+        public static final int board2=0x7f020003;
+        public static final int board3=0x7f020004;
+        public static final int board4=0x7f020005;
+        public static final int board5=0x7f020006;
+        public static final int ic_action_search=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int invishitbox100x200=0x7f020009;
+        public static final int mainmenu=0x7f02000a;
+        public static final int menuslider=0x7f02000b;
+        public static final int redslider=0x7f02000c;
+        public static final int vishitbox100x200=0x7f02000d;
     }
     public static final class id {
         public static final int graphics_holder=0x7f070000;
         public static final int menu_settings=0x7f070001;
     }
     public static final class layout {
-        public static final int activity_main_menu=0x7f030000;
+        public static final int activity_game=0x7f030000;
+        public static final int activity_main_menu=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main_menu=0x7f060000;
+        public static final int activity_game=0x7f060000;
+        public static final int activity_main_menu=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int title_activity_game=0x7f040004;
         public static final int title_activity_main_menu=0x7f040003;
     }
     public static final class style {
--- src/com/example/shuffleboard/MainMenuActivity.java
+++ src/com/example/shuffleboard/MainMenuActivity.java
@@ -1,23 +1,30 @@
 package com.example.shuffleboard;
 
 
-import java.util.ArrayList;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.Window;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class MainMenuActivity extends Activity {
-	private Target selected = null;
+	private Target selected;
 	private Target MenuPuck;
 	private Target Background;
-	private ArrayList<Point> VelocityStack = new ArrayList<Point>(5);
-	public static final int height = 775;
+	private Target NewGame;
+	private Target Options;
+	private Target HowToPlay;
+	private Target EXIT;
+	private Point PreviousLocation;
+	public static final int height = 750;
 	public static final int heightOffset = 100;
-	public static final int width = 500;
+	public static final int width = 480;
+	public static final int friction = 10;
+	public static final int titleSpaceHeight = 150;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,8 +36,14 @@ public class MainMenuActivity extends Activity {
         setContentView(R.layout.activity_main_menu);
         FrameLayout frame = (FrameLayout) findViewById(R.id.graphics_holder);
         
+        selected = null;
     	MenuPuck = new Target(this);
     	Background = new Target(this);
+    	NewGame = new Target(this);
+    	Options = new Target(this);
+    	HowToPlay = new Target(this);
+    	EXIT = new Target(this);
+    	PreviousLocation = new Point();
         
 		Background.setBitmap(R.drawable.mainmenu);
 		Background.setX(0);
@@ -42,6 +55,25 @@ public class MainMenuActivity extends Activity {
 		MenuPuck.setY(height/2 - MenuPuck.getheight()/2 + heightOffset);
 		frame.addView(MenuPuck);
 		
+		NewGame.setBitmap(R.drawable.vishitbox100x200); //this doesn't actually need to be displayed, it is to be used as the bounds for the hitbox
+		NewGame.setX(0);
+		NewGame.setY(titleSpaceHeight);
+		frame.addView(NewGame);
+		
+		Options.setBitmap(R.drawable.vishitbox100x200);
+		Options.setX(width/2 + 40);
+		Options.setY(titleSpaceHeight);
+		frame.addView(Options);
+		
+		HowToPlay.setBitmap(R.drawable.vishitbox100x200);
+		HowToPlay.setX(0);
+		HowToPlay.setY(height - HowToPlay.getheight());
+		frame.addView(HowToPlay);
+		
+		EXIT.setBitmap(R.drawable.vishitbox100x200);
+		EXIT.setX(width/2 + 50);
+		EXIT.setY(height - HowToPlay.getheight());
+		frame.addView(EXIT);
     }
 
     @Override
@@ -55,33 +87,114 @@ public class MainMenuActivity extends Activity {
 	public boolean onTouchEvent(MotionEvent event) { 
 		FrameLayout frame = (FrameLayout) findViewById(R.id.graphics_holder);
 		float X = event.getX();
-		float Y = event.getY()-48;
+		float Y = event.getY()-(MenuPuck.getheight()/2);
 		
 		if(event.getAction() == android.view.MotionEvent.ACTION_DOWN){
-			System.out.println("action down triggered");
+			System.out.println("X: " + X + " Y: " + Y);
+			System.out.println("PX: " + MenuPuck.getX() + " PY: " + MenuPuck.getY());
 			if(MenuPuck.isInside(X,Y)){
-				selected = MenuPuck;
 				System.out.println("action down triggered");
+				selected = MenuPuck;
+				selected.setX(X - selected.getwidth()/2);
+				selected.setY(Y - selected.getheight()/2);
 			}
 			return true;
 		} else if (event.getAction() == android.view.MotionEvent.ACTION_UP){
-			System.out.println("action up triggered");
-			if(MenuPuck.isInside(X,Y)){
-				selected = null;
+			if(selected != null){
 				System.out.println("action up triggered");
+				selected.setXvelo((int)(selected.getX() - PreviousLocation.x));
+				selected.setYvelo((int)(selected.getY() - PreviousLocation.y));
+				RunSlide();
+				checkHits();
+				selected = null;
 			}
 			return true;
 		} else if (event.getAction() == android.view.MotionEvent.ACTION_MOVE){
-			System.out.println("action move triggered");
-			if(MenuPuck.isInside(X,Y)){
-				MenuPuck.setX(X - MenuPuck.getwidth()/2);
-				MenuPuck.setY(Y - MenuPuck.getheight()/2);
+			if(selected != null){
 				System.out.println("action move triggered");
+				PreviousLocation.x = (int)selected.getX();
+				PreviousLocation.y = (int)selected.getY();
+				selected.setX(X - selected.getwidth()/2);
+				selected.setY(Y - selected.getheight()/2);
+				frame.removeView(selected);
+				frame.addView(selected);
+				
+				System.out.println("PLX: " + PreviousLocation.x + " PLY: " + PreviousLocation.y);
 			}
 			return true;
 		}
 		
 		return false; 
 	}
+    
+    
+    public void RunSlide(){
+		FrameLayout frame = (FrameLayout) findViewById(R.id.graphics_holder);
+    	while(selected.getXvelo() != 0 || selected.getYvelo() != 0){
+    		frame.removeView(selected);
+    		frame.addView(selected);
+    		System.out.println("SLIDE LOOP RUNNING VX: " + selected.getXvelo() + " VY: " + selected.getYvelo());
+    		selected.setX((float)selected.getX() + selected.getXvelo());
+    		selected.setY((float)selected.getY() + selected.getYvelo());
+    		
+    		if(selected.getXvelo() > 0){
+    			selected.setXvelo(selected.getXvelo() - friction);
+    			if(selected.getXvelo() < 0){
+    				selected.setXvelo(0);
+    			}
+    		}else if(selected.getXvelo() < 0){
+    			selected.setXvelo(selected.getXvelo() + friction);
+    			if(selected.getXvelo() > 0){
+    				selected.setXvelo(0);
+    			}
+    		}
+    		
+    		if(selected.getYvelo() > 0){
+    			selected.setYvelo(selected.getYvelo() - friction);
+    			if(selected.getYvelo() < 0){
+    				selected.setYvelo(0);
+    			}
+    		}else if(selected.getYvelo() < 0){
+    			selected.setYvelo(selected.getYvelo() + friction);
+    			if(selected.getYvelo() > 0){
+    				selected.setYvelo(0);
+    			}
+    		}
+    		System.out.print("BG");
+    		if(!selected.isContainedWithin(Background)){
+    			selected.setX(width/2 - MenuPuck.getwidth()/2);
+    			selected.setY(height/2 - MenuPuck.getheight()/2 + heightOffset);
+    			selected.setXvelo(0);
+    			selected.setYvelo(0);
+    			Toast.makeText(this, "OoB", Toast.LENGTH_SHORT).show();
+    		}
+    	}
+    }
+    
+    public void checkHits(){
+    	System.out.print("NG");
+    	if(selected.isContainedWithin(NewGame)){
+    		Toast.makeText(this, "TIME TO PLAY", Toast.LENGTH_SHORT).show();
+    		System.out.println("new game triggered");
+    		Intent myIntent = new Intent(this, GameActivity.class);
+    		startActivity(myIntent); 
+    	}
+    	System.out.print("OP");
+    	if(selected.isContainedWithin(Options)){
+    		Toast.makeText(this, "OPTIONS", Toast.LENGTH_SHORT).show();
+    		System.out.println("OPTIONS triggered");
+    	}
+    	System.out.print("HT");
+    	if(selected.isContainedWithin(HowToPlay)){
+    		Toast.makeText(this, "HOW TO SHUFFLE", Toast.LENGTH_SHORT).show();
+    		System.out.println("HOW triggered");
+    	}
+    	System.out.print("EX");
+    	if(selected.isContainedWithin(EXIT)){
+    		Toast.makeText(this, "EXIT", Toast.LENGTH_SHORT).show();
+    		System.out.println("EXIT triggered");
+    	} else {
+    	}
+    }
     
 }
